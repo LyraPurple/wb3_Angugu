92bec191326dc0e890cf2e2212cde2ae51b4c911
Laugras <lew_terramp@hotmail.fr>
2019-03-25 15:13:39 +0100

    Mise en forme du html avec l\'ingénieuse balise\: \<img [src]\="\'assets/\' \+ pizza\.image" \[alt\]\="pizza\.name"\>

--- pizzaparty/src/app/app.component.ts
+++ pizzaparty/src/app/app.component.ts
@@ -6,25 +6,35 @@ export class Pizza {
   price: number;
 }
 
-const PIZZAS: Pizza[] =[
-  {id: 1, name: 'Reine', price: 12.33},
-  {id: 2, name: '4 fromages', price: 13.99},
-  {id: 3, name: 'Orientale', price: 11.99},
-  {id: 4, name: 'Cannibale', price: 9.99},
-
+const PIZZAS: Pizza[] = [
+  { id: 1, name: 'Reine', price: 12.99, image: '01.jpg' },
+  { id: 2, name: '4 fromages', price: 13.99, image: '02.jpg' },
+  { id: 3, name: 'Orientale', price: 11.99, image: '03.jpg' },
+  { id: 4, name: 'Cannibale', price: 9.99, image: '04.jpg' }
 ];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   // template:``,
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'Mon site';
   pizza: Pizza = {
     id: 1,
     name: 'Reine',
-    price: 12.99
+    price: 12.99,
+    image: '01.jpg'
   };
   pizzaList = PIZZAS;
+
+
+  // Récupère  la pizza qu'on a cliquay (voir sur la consôwle)
+  onselect(pizza: Pizza): void{
+    console.log(pizza);
+    this.pizza = pizza;
+  }
 }
+
