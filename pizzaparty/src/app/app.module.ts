4d381844e5d638e2e7de17a0a28b3bcbc20dcfb1
Laugras <lew_terramp@hotmail.fr>
2019-03-26 11:54:29 +0100

    Avant MENU

--- pizzaparty/src/app/app.module.ts
+++ pizzaparty/src/app/app.module.ts
@@ -4,11 +4,20 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PizzaComponent } from './pizza/pizza.component';
+/* Ajout du dossier footer, 
+avec tout les éléments 
+  footer.component. 
+                    html, 
+                    scss, 
+                    spec.ts, 
+                    .ts */
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaComponent
+    PizzaComponent,
+    FooterComponent  /*Ajout du FooterComponent, déclaration de celui-ci */
   ],
   imports: [
     BrowserModule,
