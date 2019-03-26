6190017f53363b7f5ddb8eae7857fc3957088f6c
Laugras <lew_terramp@hotmail.fr>
2019-03-26 14:56:48 +0100

    Ajout de les routes

--- pizzaparty/src/app/app.component.ts
+++ pizzaparty/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core'; /* OnInit voire class AppComponent */
-import { Pizza } from './models/pizza';
+import { Component, OnInit } from '@angular/core'; 
+/* Les import de {Pizza} et {PizzaService } est déplacé à:
+src/app/pizzas-route/pizzas-route.component.ts */
 
 // 💥const PIZZAS: Pizza[]💥 -> services/pizza.service.ts
 
@@ -11,34 +12,14 @@ import { PizzaService } from './services/pizza.service';
   // template: ``,
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit { /* OnInit Debut */
+export class AppComponent { 
   title = 'Mon site';
-  selectedPizza: Pizza;
-  pizzaList: Pizza[];
 
-  constructor(private pizzaService: PizzaService) { } /* Définit dans /services/pizza.service */
-
-  ngOnInit() {
-    console.log('Je suis initialisé');
-    /* var self = this;
-    this.pizzaService.getPizzas().then(function (pizzas) {
-      self.pizzaList = pizzas;
-    }); */
-
-    this.pizzaService.getPizzasSlowly().then(pizzas => this.pizzaList = pizzas);
-  }
-
-  onSelect(pizza: Pizza): void {
-    console.log(pizza);
-    this.selectedPizza = pizza;
-  }
-}/* OnInit Debut */
-
-/* misc/index.html
-Teste et présentation de les promesses.
-Integration des Promesses (attente d'un évènement plutôt qu'Observation :écouter ne permanance )
-
-src/app/services/pizza.service.spec.ts
-describe  'PizzaService'
-
-src/app/services/pizza.service.ts */
+  // Déplacement dans src/app/pizzas-route/pizzas-route.component.ts de :
+/*   
+export class PizzasRouteComponent implements OnInit 
+@Component
+constructor(private pizzaService: PizzaService)
+ngOnInit 
+*/
+}
--- pizzaparty/src/app/app.module.ts
+++ pizzaparty/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PizzaComponent } from './pizza/pizza.component';
 import { MenuComponent } from './menu/menu.component'; /* Pour ajouter un menu */
+import { PizzasRouteComponent } from './pizzas-route/pizzas-route.component'; /*Ajout du PizzasRouteComponent*/
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
@@ -12,11 +14,15 @@ import { FooterComponent } from './footer/footer.component';
     AppComponent,
     PizzaComponent,
     FooterComponent,  /*Ajout du FooterComponent*/
-    MenuComponent     /*Ajout du MenuComponent*/
+    MenuComponent,    /*Ajout du MenuComponent*/
+    PizzasRouteComponent  /*Ajout du PizzasRouteComponent*/
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'pizzas', component: PizzasRouteComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
