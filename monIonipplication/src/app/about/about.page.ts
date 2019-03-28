79ccdf5b6f951e0585ff7161f88bca2b2fbdb65c
Laugras <lew_terramp@hotmail.fr>
2019-03-28 14:23:06 +0100

    Pouvoir naviguer rapidement entre -config -accueil -app tabs

--- monIonipplication/src/app/about/about.page.ts
+++ monIonipplication/src/app/about/about.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private navCtrl: NavController) { 
+    
+  }
+  goBack() {
+    this.navCtrl.back();
   }
 
 }
