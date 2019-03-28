eb61f377b998349c3eb0e1c55daf3cf5cbaed422
Laugras <lew_terramp@hotmail.fr>
2019-03-28 15:36:18 +0100

    Affichay un message à l'ajout d'une ville.

--- monIonipplication/src/app/settings/settings.page.ts
+++ monIonipplication/src/app/settings/settings.page.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
-
-  constructor() { }
+  city: string;
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
   }
 
+  save() {
+  /**
+   * Utilisay le service Alert Controller de Ionic
+   * https://ionicframework.com/docs/api/alert
+   * pour affichay la ville saysie dans une fônayrtre
+   * "môwdale"
+   */
+  this.createMessage();
+  }
+
+  async createMessage(){
+    const alert = await this.alertController.create({
+header :this.city,
+buttons: ['OKAY']
+    })
+  }
+  // await car c'est une Promesse, mettre await c'est comme .then
+await alert.present();
 }
