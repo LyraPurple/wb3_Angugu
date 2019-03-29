490b528bbf9b4e2c378d4b78698f52b72a3c28ae
Laugras <lew_terramp@hotmail.fr>
2019-03-29 16:23:49 +0100

    Page 2

--- cameraTime/src/app/tab2/tab2.page.ts
+++ cameraTime/src/app/tab2/tab2.page.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import {Camera} from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+photos: string[] = [];
+
+constructor(private camera: Camera) { }
+
+takePicture() {
+  this.camera.getPicture({
+  // On stocke l'image sous forme de string 
+    destinationType: function(imageData: any): void 
+  }).then(imageData >{
+this.photos.push('data:image/jpeg;base64,'+imageData);
+   });
+  } 
+}
